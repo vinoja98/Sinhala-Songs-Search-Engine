b92e7eee86c5b1eff682854a2befaf669a3e03d6
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-01-01 23:32:20 +0530

    indexing

--- server/data_management/retrieve_and_ingest_data.js
+++ server/data_management/retrieve_and_ingest_data.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
 const client = require('../elasticsearch/client');
 require('log-timestamp');
 const fs = require('fs');
-const app = express();
-
-// const URL = "../../corpus/song.json";
-const URL = "https://raw.githubusercontent.com/vinoja98/Sinhala-Songs-Search-Engine/main/corpus/song.json";
 
 router.get('/songs', async function (req, res) {
   console.log('Loading Application...');
@@ -17,12 +12,6 @@ router.get('/songs', async function (req, res) {
     try {
       console.log('Retrieving data');
 
-    //   const SONGS = await axios.get(`${URL}`, {
-    //     headers: {
-    //       'Content-Type': ['application/json', 'charset=utf-8'],
-    //     },
-    //   });
-    //   const SONGS = require("../../corpus/song.json")
       const data = fs.readFileSync(__dirname + '/../../corpus/song.json', 'utf8');
       const SONGS = JSON.parse(data);
 
